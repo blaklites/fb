1cb0d18541a2c4f14d2615c279853fbc91473b23
blaklites <blaklites@gmail.com>
2013-08-12 18:27:15 +0600

    version 0.2.0

---
+++ fb/wiring.py
@@ -0,0 +1,23 @@
+try:
+  import httplib
+except:
+	import http.client as httplib
+import urllib
+try:
+	import simplejson as json
+except:
+	import json
+
+
+#create and return instance that connect to facebook graph
+def create():
+	return  httplib.HTTPSConnection('graph.facebook.com')
+
+#Sendrequest to facebook graph
+#Return the facebook-json response converted to python object
+def send_request(req_cat, con, req_str, kwargs):
+	kwargs= urllib.urlencode(kwargs)
+	con.request(req_cat, req_str, kwargs)      #GET request to facebook graph
+	res=con.getresponse().read()		   #read response
+	return json.loads(res)                     #convert the response to python object
+	
