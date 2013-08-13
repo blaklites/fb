943534d9193081c13dd0763357c93ed474d2bb26
blaklites <blaklites@gmail.com>
2013-08-14 01:59:47 +0600

    version 0.3.0

--- fb/settings.py
+++ fb/settings.py
@@ -1,6 +1,6 @@
 #This dictionary is just for paramter validity checking.
 #It is used by the get_object_cat1 function in the request.py file
-#to ensure that the paramters passed for each category in the get_object 
+#to ensure that the paramters passed for each category in the get_object  
 #method is valid. The file is actualy intented for customization of the sdk
 #through a single settings file. Keep eye on future versions 
 get_object_cat1_param={"single":"fields", "multiple":"ids" }
