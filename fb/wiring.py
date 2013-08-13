47221beadcbff83c4ef50345e093da80756c46b2
blaklites <blaklites@gmail.com>
2013-08-13 17:54:40 +0600

    version 0.2.0

--- fb/wiring.py
+++ fb/wiring.py
@@ -13,11 +13,12 @@ except:
 def create():
 	return  httplib.HTTPSConnection('graph.facebook.com')
 
+
 #Sendrequest to facebook graph
 #Return the facebook-json response converted to python object
 def send_request(req_cat, con, req_str, kwargs):
-	kwargs= urllib.urlencode(kwargs)
-	con.request(req_cat, req_str, kwargs)      #GET request to facebook graph
+	kwargs= urllib.urlencode(kwargs)           #keep the url consistent spaces and any special characters
+	con.request(req_cat, req_str, kwargs)      #send request to facebook graph
 	res=con.getresponse().read()		   #read response
 	return json.loads(res)                     #convert the response to python object
 	
