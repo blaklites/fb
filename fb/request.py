002bcfea8fd40aef03b7681352757415df784752
blaklites <blaklites@gmail.com>
2013-08-13 17:48:17 +0600

    version 0.2.0

--- fb/request.py
+++ fb/request.py
@@ -2,9 +2,9 @@ import settings
 import wiring 
 
 
-#Constructs a "POST" URL. The function is used by the publish method 
-#First category of  "POST" url construction. Caling it first category because for 
-#publishing photos or more complex stuffs, newer fucntions may have to be added to deal with "POST". 
+#Constructs a "POST" and "DELETE" URL. The function is used by the publish and delete method
+#First category of  "POST" and "DELETE" url construction. Caling it first category because for 
+#publishing photos or more complex stuffs, newer fucntions might be added to deal with "POST". 
 def publish_cat1(method, con, token, cat, kwargs):
         req_str="/"+str( kwargs['id'] )+"/"+cat+'?'   	        #/id/category?
         del kwargs['id']
@@ -14,8 +14,8 @@ def publish_cat1(method, con, token, cat, kwargs):
 
 
 
-#Constructs the "GET" and "DELETE" URL. The functions is used by both the get_object and delete method 
-#First Category of "GET and "DELETE" URL construction. Again calling it first category because more
+#Constructs the "GET" URL. The functions is used by the get_object method
+#First Category of "GET" URL construction. Again calling it first category because more
 #complex functions  maybe added later. 
 def get_object_cat1(con, token, cat, kwargs):
 	req_str="/"+kwargs['id']+"?"               #/id?
