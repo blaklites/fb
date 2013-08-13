be2473dd7b057f5f3aee5868410b71953f418c40
blaklites <blaklites@gmail.com>
2013-08-13 22:37:19 +0600

    version 0.3.0

--- fb/request.py
+++ fb/request.py
@@ -1,16 +1,16 @@
-import settings
-import wiring 
+from . import settings
+from . import wiring 
 
 
 #Constructs a "POST" and "DELETE" URL. The function is used by the publish and delete method
 #First category of  "POST" and "DELETE" url construction. Caling it first category because for 
 #publishing photos or more complex stuffs, newer fucntions might be added to deal with "POST". 
 def publish_cat1(method, con, token, cat, kwargs):
-        req_str="/"+str( kwargs['id'] )+"/"+cat+'?'   	        #/id/category?
+        req_str="/"+str( kwargs['id'] )+"/"+cat+'?'                #/id/category?
         del kwargs['id']
- 	kwargs['access_token']=token 				#add access token tokwwargs
+        kwargs['access_token']=token                               #add access token to kwwargs
         res=wiring.send_request(method, con, req_str, kwargs)    
-	return res
+        return res
 
 
 
@@ -18,21 +18,21 @@ def publish_cat1(method, con, token, cat, kwargs):
 #First Category of "GET" URL construction. Again calling it first category because more
 #complex functions  maybe added later. 
 def get_object_cat1(con, token, cat, kwargs):
-	req_str="/"+kwargs['id']+"?"               #/id?
-	req_str+="access_token="+token             #/id?@acces_token=......
-	del kwargs['id']
-	
-	key=settings.get_object_cat1_param[cat]    #get the param name for the category(fields, ids)
-	req_str+="&"+key+"="                       #/id?@acces_token=......key=
-	if key in kwargs.keys():
-		length=len( kwargs[key] )
-		for i in range(length):
-			if i==0:
-				req_str+=kwargs[key][i]
-			else:
-				req_str+=","+kwargs[key][i]	
-	else:
-		return "Parameter Error"
-	
-	res=wiring.send_request("GET", con, req_str, '')
-	return res
+        req_str="/"+kwargs['id']+"?"               #/id?
+        req_str+="access_token="+token             #/id?@acces_token=......
+        del kwargs['id']
+        
+        key=settings.get_object_cat1_param[cat]    #get the param name for the category(single, multiple)
+        req_str+="&"+key+"="                       #/id?@acces_token=......key=
+        if key in kwargs.keys():
+                length=len( kwargs[key] )
+                for i in range(length):
+                        if i==0:
+                                req_str+=kwargs[key][i]
+                        else:
+                                req_str+=","+kwargs[key][i]        
+        else:
+                return "Parameter Error"
+        
+        res=wiring.send_request("GET", con, req_str, '')
+        return res
