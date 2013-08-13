3ad3f51abbd68c121eb6e61b712762c13084cca7
blaklites <blaklites@gmail.com>
2013-08-13 17:44:49 +0600

    version 0.2.0

--- fb/request.py
+++ fb/request.py
@@ -1,10 +1,10 @@
 import settings
 import wiring 
 
-#Constructs the "POST" URL
-#First category of publish. Caling it first category because for publishing photos or more complex stuffs,
-#might have to code another publish method. The function is used by the publish method 
 
+#Constructs a "POST" URL. The function is used by the publish method 
+#First category of  "POST" url construction. Caling it first category because for 
+#publishing photos or more complex stuffs, newer fucntions may have to be added to deal with "POST". 
 def publish_cat1(method, con, token, cat, kwargs):
         req_str="/"+str( kwargs['id'] )+"/"+cat+'?'   	        #/id/category?
         del kwargs['id']
@@ -13,17 +13,17 @@ def publish_cat1(method, con, token, cat, kwargs):
 	return res
 
 
-#Constructs the "GET" and "DELETE" URL
-#First Category of get method. Again calling it first category because more complex methods maybe added later
-#The functions is used by both the get_object and delete method 
 
+#Constructs the "GET" and "DELETE" URL. The functions is used by both the get_object and delete method 
+#First Category of "GET and "DELETE" URL construction. Again calling it first category because more
+#complex functions  maybe added later. 
 def get_object_cat1(con, token, cat, kwargs):
-	req_str="/"+kwargs['id']+"?"               #/{id}?
-	req_str+="access_token="+token             #/{id}?@acces_token=......
+	req_str="/"+kwargs['id']+"?"               #/id?
+	req_str+="access_token="+token             #/id?@acces_token=......
 	del kwargs['id']
 	
 	key=settings.get_object_cat1_param[cat]    #get the param name for the category(fields, ids)
-	req_str+="&"+key+"="                       #/{id}?@acces_token=......{key}=
+	req_str+="&"+key+"="                       #/id?@acces_token=......key=
 	if key in kwargs.keys():
 		length=len( kwargs[key] )
 		for i in range(length):
