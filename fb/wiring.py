1d0e93caebf0a32b61adfba0eaf604fa599312c0
Samir Ahmed <blaklites@gmail.com>
2014-04-08 12:25:44 +0600

    Update Docstring

--- fb/wiring.py
+++ fb/wiring.py
@@ -11,23 +11,25 @@ try:
 except:
 	import json
 
-
-#create and return instance that connect to facebook graph
+"""
+create and return instance that connect to facebook graph
+"""
 def create():
 	return  httplib.HTTPSConnection('graph.facebook.com')
 
-
-#Sends request to facebook graph
-#Returns the facebook-json response converted to python object
+"""
+Sends request to facebook graph
+Returns the facebook-json response converted to python object
+"""
 def send_request(req_cat, con, req_str, kwargs):
         try:
-                kwargs= parse.urlencode(kwargs)    #python3x
+                kwargs = parse.urlencode(kwargs)    #python3x
         except:
-                kwargs= urllib.urlencode(kwargs)   #python2x
+                kwargs = urllib.urlencode(kwargs)   #python2x
         con.request(req_cat, req_str, kwargs)      #send request to facebook graph
-        res=con.getresponse().read()		   #read response
-        t=type(res)
+        res = con.getresponse().read()		   #read response
+        t = type(res)
         if type(res) == t:
-                res=bytes.decode(res)
+                res = bytes.decode(res)
         return json.loads(res)                     #convert the response to python object
  
