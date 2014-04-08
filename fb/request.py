99a4d2d9ea49197e15dc247b4688096757ee412c
Samir Ahmed <blaklites@gmail.com>
2014-04-08 12:30:40 +0600

    Update request.py

--- fb/request.py
+++ fb/request.py
@@ -2,12 +2,13 @@ from . import settings
 from . import wiring  
 
 
-"""
-Constructs a "POST" and "DELETE" URL. The function is used by the publish and delete method
-First category of  "POST" and "DELETE" url construction. Caling it first category because for 
-publishing photos or more complex stuffs, newer fucntions might be added to deal with "POST". 
-"""
+
 def publish_cat1(method, con, token, cat, kwargs):
+        """
+        Constructs a "POST" and "DELETE" URL. The function is used by the publish and delete method
+        First category of  "POST" and "DELETE" url construction. Caling it first category because for 
+        publishing photos or more complex stuffs, newer fucntions might be added to deal with "POST". 
+        """
         req_str = "/"+str( kwargs['id'] )+"/"+cat+'?'                #/id/category?
         del kwargs['id']
         kwargs['access_token'] = token                               #add access token to kwwargs
@@ -15,12 +16,13 @@ def publish_cat1(method, con, token, cat, kwargs):
         return res
 
 
-"""
-Constructs the "GET" URL. The functions is used by the get_object method
-First Category of "GET" URL construction. Again calling it first category because more
-complex functions  maybe added later. 
-"""
+
 def get_object_cat1(con, token, cat, kwargs):
+        """
+        Constructs the "GET" URL. The functions is used by the get_object method
+        First Category of "GET" URL construction. Again calling it first category because more
+        complex functions  maybe added later. 
+        """
         req_str = "/"+kwargs['id']+"?"               #/id?
         req_str+ = "access_token="+token             #/id?@acces_token=......
         del kwargs['id']
