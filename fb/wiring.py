f52d09ad8a118e93ede9f2d778962d84bcb67262
Horacio Ibrahim <horacioibrahim@gmail.com>
2014-12-28 22:26:19 -0200

    Wrapper to keep connection ESTABLISHED, before an not expected CLOSE_WAIT

--- fb/wiring.py
+++ fb/wiring.py
@@ -27,10 +27,27 @@ def send_request(req_cat, con, req_str, kwargs):
                 kwargs = parse.urlencode(kwargs)    #python3x
         except:
                 kwargs = urllib.urlencode(kwargs)   #python2x
-        con.request(req_cat, req_str, kwargs)      #send request to facebook graph
-        res = con.getresponse().read()		   #read response
+        
+        """
+        Wrapper to keep TCP connection ESTABLISHED. Rather the connection go to
+        CLOSE_WAIT and raise errors CannotSendRequest or the server reply with
+        empty and it raise BadStatusLine
+        """        
+        try:
+            con.request(req_cat, req_str, kwargs)      #send request to facebook graph
+        except httplib.CannotSendRequest:
+            con = create()
+            con.request(req_cat, req_str, kwargs)
+
+        try:
+            res = con.getresponse().read()		   #read response
+        except (IOError, httplib.BadStatusLine):
+            con = create()
+            con.request(req_cat, req_str, kwargs) 
+            res = con.getresponse().read()  
+
         t = type(res)
         if type(res) == t:
                 res = bytes.decode(res)
         return json.loads(res)                     #convert the response to python object
- 
+
